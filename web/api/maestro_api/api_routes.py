411a1c15973b22f75468ce2dfae8edeeed90c033
Vitalii Melnychuk <vvv.melnychuk@gmail.com>
2021-12-15 10:10:37 +0000

    feat: add run_status update routes (#4)
    * feat: add run_status update routes
    
    * chore: linting
    
    * feat: update swagger with run_status update endpoints
    
    * feat: add run_status endpoints tests
    
    * feat: add code coverage check commands
    
    * feat: add run_status tests
    
    * chore: stop words

--- web/api/maestro_api/api_routes.py
+++ web/api/maestro_api/api_routes.py
@@ -1,10 +1,12 @@
 from maestro_api.controllers.run import RunController
+from maestro_api.controllers.run_status import RunStatusController
 from maestro_api.validation_schemas import create_run_schema, update_run_schema
 from maestro_api.libs.flask.decorators import requires_auth, validate_request
 
 
 def init_api_routes(flask_app):
     run_controller = RunController(flask_app)
+    run_status_controller = RunStatusController(flask_app)
 
     # /run routes
     @flask_app.route("/run/<run_id>", methods=["DELETE"])
@@ -33,3 +35,19 @@ def init_api_routes(flask_app):
     @requires_auth()
     def run_all(*args, **kwargs):
         return run_controller.all(*args, **kwargs)
+
+    # TODO: change to single endpoint from where we can change run status
+    @flask_app.route("/run_status/<run_id>/start", methods=["POST"])
+    @requires_auth()
+    def run_status_start(*args, **kwargs):
+        return run_status_controller.start_one(*args, **kwargs)
+
+    @flask_app.route("/run_status/<run_id>/stop", methods=["POST"])
+    @requires_auth()
+    def run_status_stop(*args, **kwargs):
+        return run_status_controller.stop_one(*args, **kwargs)
+
+    @flask_app.route("/run_status/<run_id>/finish", methods=["POST"])
+    @requires_auth()
+    def run_status_finish(*args, **kwargs):
+        return run_status_controller.finish_one(*args, **kwargs)
