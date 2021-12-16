aa47c6c0b1c576f3c6f950a3a7282ce30f909fdc
Vitalii Melnychuk <vitalii.melnychuk@farfetch.com>
2021-12-16 16:42:18 +0000

    feat: init react frontend (#17)
    * feat: init react.js maestro frontend
    
    * feat: use node v16.13.1
    
    * feat: update Readme file

---
+++ web/frontend/.eslintrc.js
@@ -0,0 +1,47 @@
+module.exports = {
+  env: {
+    node: true,
+    mocha: true
+  },
+  extends: [
+    "react-app",
+    "airbnb-base",
+    "prettier",
+    "plugin:import/errors",
+    "plugin:import/warnings"
+  ],
+  plugins: ["prettier", "simple-import-sort"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
+  },
+  parserOptions: {
+    ecmaVersion: 11
+  },
+  rules: {
+    "prettier/prettier": "error",
+    "no-console": 1,
+    "simple-import-sort/imports": "error",
+    "simple-import-sort/exports": "error",
+    "import/prefer-default-export": 0,
+    "no-unused-vars": "warn",
+    // Code complexity rules
+    complexity: ["error", 10],
+    "max-depth": ["error", 3],
+    "max-nested-callbacks": ["error", 3],
+    "max-statements": ["error", 10],
+    "max-lines": ["error", 300]
+  },
+  overrides: [
+    // Code complexity rules for test files
+    {
+      files: ["*.test.js"],
+      rules: {
+        "max-depth": ["error", 2],
+        "max-nested-callbacks": ["error", 4],
+        "max-statements": ["error", 30],
+        "max-lines": ["error", 500]
+      }
+    }
+  ]
+};
