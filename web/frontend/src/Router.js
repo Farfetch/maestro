6f53b5743e509eb0d80b2d7e6cc8de71c8a64519
Vitalii Melnychuk <vitalii.melnychuk@farfetch.com>
2021-12-16 17:52:14 +0000

    feat: bump react-scripts to v5 and react-router-dom to v6 (#19)
    * feat: add dependabot frontend weekly updates
    
    * feat: add Github action for frontend
    
    * chore: rename job
    
    * fix: add yarn lock path path
    
    * fix: path to lock file
    
    * feat: use one file for all CI jobs
    
    * feat: add tests running check
    
    * feat: bump react-scripts to v5
    
    * feat: upgrade react-router-dom to v6

--- web/frontend/src/Router.js
+++ web/frontend/src/Router.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import HomePage from "./pages/Home";
 import NotFoundPage from "./pages/NotFound";
@@ -7,15 +7,10 @@ import NotFoundPage from "./pages/NotFound";
 const Router = ({ children }) => (
   <BrowserRouter>
     {children}
-    <Switch>
-      <Route exact path="/">
-        <HomePage />
-      </Route>
-
-      <Route>
-        <NotFoundPage />
-      </Route>
-    </Switch>
+    <Routes>
+      <Route path="/" element={<HomePage />}></Route>
+      <Route path="*" element={<NotFoundPage />}></Route>
+    </Routes>
   </BrowserRouter>
 );
 
