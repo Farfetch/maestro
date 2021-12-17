e88ee8211ef600dcc52e097a1c26a2fbf83a5bdc
Vitalii <vitalii.melnychuk@farfetch.com>
2021-12-17 13:06:28 +0000

    feat: update Layout component tests

--- web/frontend/src/pages/Layout/Layout.test.js
+++ web/frontend/src/pages/Layout/Layout.test.js
@@ -1,3 +1,18 @@
-it("Layout", async () => {
-  expect("hello").toBe("hello");
+import { render } from "@testing-library/react";
+import React from "react";
+import { MemoryRouter } from "react-router-dom";
+
+import Layout from "./Layout";
+
+describe("pages/Layout", () => {
+  test(`should render Layout component`, async () => {
+    const rendered = render(
+      <MemoryRouter initialEntries={["/"]}>
+        <Layout />
+      </MemoryRouter>
+    );
+
+    const badgeComponent = rendered.container;
+    expect(badgeComponent.outerHTML).toMatchSnapshot();
+  });
 });
