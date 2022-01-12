80fe172e3f9e13f81f11152bfbc01d3bd46fe3fb
Vitalii Melnychuk <vitalii.melnychuk@farfetch.com>
2022-01-12 16:01:07 +0000

    feat: add endpoint to restart the test (#87)
    * feat: add PUT run_status/{run_id}/restart to restart test
    
    * feat" update docs with restart test endpoint
    
    * feat: validate states from where we can stop test

--- web/api/maestro_api/controllers/run_status.py
+++ web/api/maestro_api/controllers/run_status.py
@@ -1,4 +1,4 @@
-from maestro_api.db.models.event import Event, EventType
+from maestro_api.db.models.event import Event, EventType, EventStatus
 from maestro_api.db.models.run import Run, RunStatus
 
 from maestro_api.libs.flask.utils import (
@@ -12,67 +12,107 @@ class RunStatusController:
     def __init__(self, flask_app):
         self.flask_app = flask_app
 
+    def create_agent_events(self, run_id, agents):
+
+        all_events = []
+
+        for agent in agents:
+            for agent_id in agent["ids"]:
+                event = Event(
+                    event_type=agent["event_type"],
+                    run_id=run_id,
+                    agent_id=agent_id,
+                ).save()
+                all_events.append(event)
+
+        return all_events
+
     def start_one(self, run_id, user):
         """
-        Start test run based on configuration
+        Start test run based on ID
+
+        Available only for Runs with status: PENDING
         """
         run = get_obj_or_404(Run, id=run_id)
 
-        if run.run_status == RunStatus.RUNNING.value:
-            return bad_request_response("Run is already running")
+        # Only tests in PENDING state can be started
+        if run.run_status != RunStatus.PENDING.value:
+            return bad_request_response(
+                "Run status is not '%s'" % RunStatus.PENDING.value
+            )
+        server_agents = {
+            "ids": run.server_agent_ids,
+            "event_type": EventType.START_SERVER_AGENT.value,
+        }
+        client_agents = {
+            "ids": [run.client_agent_id],
+            "event_type": EventType.START_RUN.value,
+        }
+        all_events = self.create_agent_events(
+            run.id,
+            [server_agents, client_agents],
+        )
 
-        all_events = []
-        # Create Events to start server agents
-        for server_agent_id in run.server_agent_ids:
-            event = Event(
-                event_type=EventType.START_SERVER_AGENT.value,
-                run_id=run.id,
-                agent_id=server_agent_id,
-            ).save()
-            all_events.append(event)
+        return jsonify_list_of_docs(all_events)
 
-        # Create Event to start test
-        start_run_event = Event(
-            event_type=EventType.START_RUN.value,
-            run_id=run.id,
-            agent_id=run.client_agent_id,
-        ).save()
+    def restart_one(self, run_id, user):
+        """
+        Restart test run based on RunId.
 
-        all_events.append(start_run_event)
+        Execution metrics would be removed by client agent.
+        """
+        run = get_obj_or_404(Run, id=run_id)
 
-        # Update Test run status to avoid generating a lot of events
-        # The next status would be "RUNNING" setted up by agents
-        run.run_status = RunStatus.PENDING.value
-        run.save()
+        server_agents = {
+            "ids": run.server_agent_ids,
+            "event_type": EventType.RESTART_SERVER_AGENT.value,
+        }
+        client_agents = {
+            "ids": [run.client_agent_id],
+            "event_type": EventType.RESTART_RUN.value,
+        }
+        all_events = self.create_agent_events(
+            run.id,
+            [server_agents, client_agents],
+        )
 
         return jsonify_list_of_docs(all_events)
 
     def stop_one(self, run_id, user):
-        """Stop run that is in progress"""
+        """
+        Stop execution of Run
+
+        Available only for Runs with status: PENDING CREATING, RUNNING
+        Update RunStatus to STOPPED
+        """
         run = get_obj_or_404(Run, id=run_id)
 
-        if run.run_status != RunStatus.RUNNING.value:
+        available_statuses = [
+            RunStatus.PENDING.value,
+            RunStatus.CREATING.value,
+            RunStatus.RUNNING.value,
+        ]
+
+        if run.run_status not in available_statuses:
             return bad_request_response(
-                "Run status is not '%s'" % RunStatus.RUNNING.value
+                "Run status should be one of %s" % str(available_statuses)
             )
 
-        all_events = []
-        # Create Event to stop server agents
-        for server_agent_id in run.server_agent_ids:
-            event = Event(
-                event_type=EventType.STOP_SERVER_AGENT.value,
-                run_id=run.id,
-                agent_id=server_agent_id,
-            ).save()
-            all_events.append(event)
+        # New Events to stop test would be created
+        Event.objects(run_id=run_id, event_status=EventStatus.PENDING.value).delete()
 
-        # Create Event to stop test
-        start_test_event = Event(
-            event_type=EventType.STOP_RUN.value,
-            run_id=run.id,
-            agent_id=run.client_agent_id,
-        ).save()
-        all_events.append(start_test_event)
+        server_agents = {
+            "ids": run.server_agent_ids,
+            "event_type": EventType.STOP_SERVER_AGENT.value,
+        }
+        client_agents = {
+            "ids": [run.client_agent_id],
+            "event_type": EventType.STOP_RUN.value,
+        }
+        all_events = self.create_agent_events(
+            run.id,
+            [server_agents, client_agents],
+        )
 
         run.run_status = RunStatus.STOPPED.value
         run.save()
@@ -82,24 +122,27 @@ class RunStatusController:
     def finish_one(self, run_id, user):
         """
         Send event that test is finished to server agents.
-        Updates Run status to "FINISHED"
+
+        Available only for Runs with status: RUNNING
+        Update RunStatus to FINISHED
         """
         run = get_obj_or_404(Run, id=run_id)
 
+        # Only running tests can go to the FINISHED state
         if run.run_status != RunStatus.RUNNING.value:
             return bad_request_response(
                 "Run status is not '%s'" % RunStatus.RUNNING.value
             )
 
-        all_events = []
-        # Create Event to stop server agents
-        for server_agent_id in run.server_agent_ids:
-            event = Event(
-                event_type=EventType.STOP_SERVER_AGENT.value,
-                run_id=run.id,
-                agent_id=server_agent_id,
-            ).save()
-            all_events.append(event)
+        server_agents = {
+            "ids": run.server_agent_ids,
+            "event_type": EventType.STOP_SERVER_AGENT.value,
+        }
+
+        all_events = self.create_agent_events(
+            run.id,
+            [server_agents],
+        )
 
         run.run_status = RunStatus.FINISHED.value
         run.save()
