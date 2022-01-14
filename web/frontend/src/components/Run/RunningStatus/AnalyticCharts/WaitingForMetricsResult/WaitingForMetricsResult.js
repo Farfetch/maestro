839e618052e834ffdbf07f5de47655a674b23057
Vitalii Melnychuk <vitalii.melnychuk@farfetch.com>
2022-01-14 17:30:24 +0000

    feat: add test running loader state

---
+++ web/frontend/src/components/Run/RunningStatus/AnalyticCharts/WaitingForMetricsResult/WaitingForMetricsResult.js
@@ -0,0 +1,26 @@
+import { Result, Statistic } from "antd";
+
+import CircleSpinner from "../../../../layout/CircleSpinner";
+import Timer from "../../../../Timer";
+import StopExecutionButton from "../../StopExecutionButton";
+
+const WaitingForMetricsResult = ({ run }) => (
+  <Result
+    icon={<CircleSpinner size={48} />}
+    title="Almost there! Metrics will be available in a minute"
+    subTitle={
+      <>
+        Test is already started and running. Usualy is takes takes 1-2 minutes,
+        please wait...
+        <Timer startTime={run.createdAt} defaultTime={run.createdAt}>
+          {(time, loading) => (
+            <Statistic title="" value={time} loading={loading} />
+          )}
+        </Timer>
+      </>
+    }
+    extra={[<StopExecutionButton runId={run.id} key="stopExecution" />]}
+  />
+);
+
+export default WaitingForMetricsResult;
