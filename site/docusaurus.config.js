c5581486458b0eab42905ffbd1c085c0deadb713
Vitalii Melnychuk <vitalii.melnychuk@farfetch.com>
2022-02-04 11:11:52 +0000

    fix: edit documentation edit link

--- site/docusaurus.config.js
+++ site/docusaurus.config.js
@@ -24,9 +24,8 @@ const config = {
         docs: {
           path: "../docs",
           sidebarPath: require.resolve("./sidebars.js"),
-          // Please change this to your repo.
           editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+            "https://github.com/Farfetch/maestro/tree/master/docs",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
