57bc63c861035f0659835386db3ee598d31aaaaf
Vitalii Melnychuk <vitalii.melnychuk@farfetch.com>
2022-03-02 14:06:35 +0000

    fest: add `Next Run` column for tests list table (#251)
    * fix: schedule time storing format
    
    * feat: update run configuration table with next run

--- web/frontend/src/components/RunConfiguration/Table/RunConfigurationsTable.test.js
+++ web/frontend/src/components/RunConfiguration/Table/RunConfigurationsTable.test.js
@@ -25,8 +25,8 @@ describe("components/RunConfiguration/Table", () => {
       </MemoryRouter>
     );
 
-    const badgeComponent = rendered.container;
-    expect(badgeComponent.outerHTML).toMatchSnapshot();
+    const runConfigurationTableComponent = rendered.container;
+    expect(runConfigurationTableComponent.outerHTML).toMatchSnapshot();
   });
 
   test(`should render RunConfigurationTable loader`, async () => {
@@ -39,7 +39,56 @@ describe("components/RunConfiguration/Table", () => {
       </MemoryRouter>
     );
 
-    const badgeComponent = rendered.container;
-    expect(badgeComponent.outerHTML).toMatchSnapshot();
+    const runConfigurationTableComponent = rendered.container;
+    expect(runConfigurationTableComponent.outerHTML).toMatchSnapshot();
+  });
+
+  test(`should render RunConfigurationTable nextRun column`, async () => {
+    const runConfigurationsWithSchedule = [
+      {
+        id: "run-configuration-id",
+        title: "Confgiuration title",
+        runPlanId: "run-plan-id",
+        schedule: {
+          days: ["Thu"],
+          time: "13:50"
+        },
+        createdAt: moment("2018-05-24T13:48:04.313000"),
+        updatedAt: moment("2018-05-24T13:48:04.313000")
+      },
+      {
+        id: "run-configuration-id",
+        title: "Confgiuration title",
+        runPlanId: "run-plan-id",
+        schedule: {
+          days: ["Thu"],
+          time: "13:00"
+        },
+        createdAt: moment("2018-05-24T13:48:04.313000"),
+        updatedAt: moment("2018-05-24T13:48:04.313000")
+      },
+      {
+        id: "run-configuration-id",
+        title: "Confgiuration title",
+        runPlanId: "run-plan-id",
+        schedule: {
+          days: ["Sun"],
+          time: "10:00"
+        },
+        createdAt: moment("2018-05-24T13:48:04.313000"),
+        updatedAt: moment("2018-05-24T13:48:04.313000")
+      }
+    ];
+    const rendered = render(
+      <MemoryRouter initialEntries={["/tests"]}>
+        <RunConfigurationsTable
+          runConfigurations={runConfigurationsWithSchedule}
+          isLoading={false}
+        />
+      </MemoryRouter>
+    );
+
+    const runConfigurationTableComponent = rendered.container;
+    expect(runConfigurationTableComponent.outerHTML).toMatchSnapshot();
   });
 });
