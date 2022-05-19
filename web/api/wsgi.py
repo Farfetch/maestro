d8c9384d0236caacdd73d2e5f22a44b0808bbca3
Vitalii Melnychuk <vitalii.melnychuk@farfetch.com>
2022-05-19 09:31:57 +0100

    feat: ability to enable scheduler with api

--- web/api/wsgi.py
+++ web/api/wsgi.py
@@ -1,9 +1,11 @@
 from werkzeug.middleware.dispatcher import DispatcherMiddleware
+from pytz import UTC
+from apscheduler.schedulers.background import BackgroundScheduler
 
 from maestro_api import create_app, create_frontend_app
 from maestro_api.db.mongo import init_db_data
 from maestro_api import settings
-
+from maestro_api.scheduler import init_scheduler
 from maestro_api.logging import Logger
 
 Logger.setup_logging()
@@ -17,3 +19,8 @@ init_db_data(maestro_api)
 maestro_app.wsgi_app = DispatcherMiddleware(
     maestro_app.wsgi_app, {"/api": maestro_api.wsgi_app}
 )
+
+
+if settings.SCHEDULER_ENABLED:
+    scheduler = BackgroundScheduler(timezone=UTC)
+    init_scheduler(scheduler)
