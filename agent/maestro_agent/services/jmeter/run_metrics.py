085761200b134bafde556a3f56521306e5776e14
dyego.carvalho <dyego.carvalho@farfetch.com>
2023-02-28 11:38:20 +0000

    feat: Add multi-thread to metrics processor

--- agent/maestro_agent/services/jmeter/run_metrics.py
+++ agent/maestro_agent/services/jmeter/run_metrics.py
@@ -8,7 +8,10 @@ from maestro_agent.services.maestro_api.run import RunApi
 from maestro_agent.libs.csv.file_observer import CsvFileObserver
 from maestro_agent.libs.utils import chunks
 from maestro_agent.logging import Logger
-from maestro_agent.settings import MAESTRO_METRICS_PROCESSING_BULK_SIZE
+from maestro_agent.settings import (
+    MAESTRO_METRICS_PROCESSING_BULK_SIZE,
+    MAESTRO_METRICS_PROCESSING_WORKERS,
+)
 
 
 class RunMetricsProcessor:
@@ -20,8 +23,8 @@ class RunMetricsProcessor:
     """
 
     BULK_SIZE = MAESTRO_METRICS_PROCESSING_BULK_SIZE
-    PROCESSIGNG_WORKERS = 10
-    PROCESSIGNG_SECONDS_INTERVAL = 5
+    PROCESSING_WORKERS = MAESTRO_METRICS_PROCESSING_WORKERS
+    PROCESSING_SECONDS_INTERVAL = 5
     run_id = None
 
     last_send_date = datetime.now()
@@ -41,7 +44,13 @@ class RunMetricsProcessor:
             self.init_threads()
 
     def init_threads(self):
-        threads = [Thread(target=self.processing_worker)]
+        Logger.debug(f"Number of workers processing metrics to sent to API: {self.PROCESSING_WORKERS}")
+        Logger.debug(f"Time based processor to send metrics to Queue (local): {self.PROCESSING_SECONDS_INTERVAL}")
+        
+        threads = [
+            Thread(target=self.processing_worker)
+            for elem in range(self.PROCESSING_WORKERS)
+        ]
         threads.append(Thread(target=self.time_based_processor))
         self.threads = threads
 
@@ -80,7 +89,7 @@ class RunMetricsProcessor:
     def add_data(self, data):
         self.data.append(data)
 
-    def send_data(self, skip_seconds=0):
+    def send_data_to_queue(self, skip_seconds=0):
         data = self.data.copy()
 
         (metrics_to_process, skipped_metrics) = self.split_by_last_seconds(
@@ -97,22 +106,21 @@ class RunMetricsProcessor:
         while self.run_processing_workers is True:
             current_time = datetime.now()
             seconds_diff = (current_time - self.last_send_date).total_seconds()
-            if seconds_diff > 5:
-                self.send_data(skip_seconds=1)
+            if seconds_diff > self.PROCESSING_SECONDS_INTERVAL:
+                self.send_data_to_queue(skip_seconds=1)
 
             sleep(0.5)
 
     def processing_worker(self):
         while self.run_processing_workers is True:
-
             if self.queue.empty() is False:
-
                 data = self.queue.get()
                 for data_chunk in chunks(data, self.BULK_SIZE):
                     self.last_send_date = datetime.now()
                     try:
                         RunApi.send_metrics(run_id=self.run_id, metrics=data_chunk)
-                        Logger.debug(f"{len(data_chunk)} run metrics sent"),
+                        Logger.debug(f"{len(data_chunk)} run metrics sent to API"),
+                        Logger.debug(f"{self.queue.qsize()} queue size"),
 
                     except Exception:
                         self.send_metrics_error["count"] += 1
@@ -126,7 +134,7 @@ class RunMetricsProcessor:
         self.add_data(data)
 
         if len(self.data) >= self.BULK_SIZE:
-            self.send_data(skip_seconds=1)
+            self.send_data_to_queue(skip_seconds=1)
 
         self.log_metrics_send_errors()
 
@@ -168,10 +176,10 @@ class RunMetricsCsvFileObserver(CsvFileObserver):
 
     def on_finish(self):
         """
-        Alaways clean up the queue and send metrics once the file process finish
+        Always clean up the queue and send metrics once the file process finish
         """
         Logger.debug("Processing metrics file finished")
-        self.processor.send_data()
+        self.processor.send_data_to_queue()
 
         while self.processor.queue.empty() is False:
             sleep(1)
