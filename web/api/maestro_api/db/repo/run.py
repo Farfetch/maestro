98b210c59a3cf45e670c66d83d03e599c87fff68
crazyplayy <48414755+crazyplayy@users.noreply.github.com>
2023-08-03 10:48:33 +0100

    Feat: Tokens can be used to dynamically change title (#767)

--- web/api/maestro_api/db/repo/run.py
+++ web/api/maestro_api/db/repo/run.py
@@ -5,11 +5,44 @@ from maestro_api.db.models.run_agent import RunAgent
 from maestro_api.db.models.run_metric import RunMetric
 from maestro_api.db.models.run_metric_label import RunMetricLabel
 
+import re
+
 
 class RunRepository:
     """
     Repository to manage common opperation over run and related agents
     """
+
+    def parse_title(self, title, num_agents, custom_properties, max_rps):
+        token_regex = r"\{(.+?)\}"
+
+        has_tokens = bool(re.search(token_regex, title))
+        if not has_tokens:
+            return title
+
+        token_replacements = {
+            "NUM_AGENTS": num_agents,
+            "MAX_RPS": "" if max_rps == 0 else max_rps,
+        }
+
+        def replace_token(match):
+            token = match.group(1)
+            replacement = token_replacements.get(token)
+
+            if replacement is not None:
+                return str(replacement)
+
+            custom_property = next(
+                (prop for prop in custom_properties if prop["name"] == token), None
+            )
+            if custom_property:
+                return str(custom_property["value"])
+
+            return match.group(0)
+
+        parsed_title = re.sub(token_regex, replace_token, title)
+
+        return parsed_title
 
     def create_run(self, run_configuration: RunConfiguration):
         hosts = [host.to_mongo() for host in run_configuration.hosts]
@@ -23,8 +56,19 @@ class RunRepository:
             for custom_property in run_configuration.custom_properties
         ]
 
+        max_rps = 0
+        if run_configuration.is_load_profile_enabled and load_profile:
+            max_rps = max(max(item["start"], item["end"]) for item in load_profile)
+
+        parsed_title = self.parse_title(
+            run_configuration.title,
+            len(run_configuration.agent_ids),
+            run_configuration.custom_properties,
+            max_rps,
+        )
+
         new_run = Run(
-            title=run_configuration.title,
+            title=parsed_title,
             run_configuration_id=run_configuration.id,
             workspace_id=run_configuration.workspace_id,
             agent_ids=run_configuration.agent_ids,
---
+++ web/api/tests/db/repo/test_RunRepository_parse_title.py
@@ -0,0 +1,84 @@
+from maestro_api.db.repo.run import RunRepository
+
+
+def test_parse_title():
+    title = "SS23 Portal {MAX_RPS}x{NUM_AGENTS} - Duration:{duration}"
+    num_agents = 1
+    custom_properties = [{"name": "duration", "value": "600"}]
+    max_rps = 100
+    parsed_title = RunRepository().parse_title(
+        title, num_agents, custom_properties, max_rps
+    )
+
+    expected_title = "SS23 Portal 100x1 - Duration:600"
+    assert parsed_title == expected_title
+
+
+def test_parse_title_multiple_custom_props():
+    title = (
+        "SS23 Portal {MAX_RPS}x{NUM_AGENTS} - Duration:{duration} - loop:{loop_value}"
+    )
+    num_agents = 1
+    custom_properties = [
+        {"name": "duration", "value": "600"},
+        {"name": "loop_value", "value": "45"},
+    ]
+    max_rps = 100
+    parsed_title = RunRepository().parse_title(
+        title, num_agents, custom_properties, max_rps
+    )
+
+    expected_title = "SS23 Portal 100x1 - Duration:600 - loop:45"
+    assert parsed_title == expected_title
+
+
+def test_parse_title_max_rps_is_zero():
+    title = (
+        "SS23 Portal {MAX_RPS}x{NUM_AGENTS} - Duration:{duration} - loop:{loop_value}"
+    )
+    num_agents = 1
+    custom_properties = [
+        {"name": "duration", "value": "600"},
+        {"name": "loop_value", "value": "45"},
+    ]
+    max_rps = 0
+    parsed_title = RunRepository().parse_title(
+        title, num_agents, custom_properties, max_rps
+    )
+
+    expected_title = "SS23 Portal x1 - Duration:600 - loop:45"
+    assert parsed_title == expected_title
+
+
+def test_parse_title_no_tokens():
+    title = "SS23 Portal - Duration: - loop:"
+    num_agents = 1
+    custom_properties = [
+        {"name": "duration", "value": "600"},
+        {"name": "loop_value", "value": "45"},
+    ]
+    max_rps = 100
+    parsed_title = RunRepository().parse_title(
+        title, num_agents, custom_properties, max_rps
+    )
+
+    expected_title = "SS23 Portal - Duration: - loop:"
+    assert parsed_title == expected_title
+
+
+def test_parse_title_invalid_token():
+    title = (
+        "SS23 Portal {MAX_RPS}x{NUM_AGENTS} - Duration:{duration} - loop:{bananinha}"
+    )
+    num_agents = 1
+    custom_properties = [
+        {"name": "duration", "value": "600"},
+        {"name": "loop_value", "value": "45"},
+    ]
+    max_rps = 100
+    parsed_title = RunRepository().parse_title(
+        title, num_agents, custom_properties, max_rps
+    )
+
+    expected_title = "SS23 Portal 100x1 - Duration:600 - loop:{bananinha}"
+    assert parsed_title == expected_title
