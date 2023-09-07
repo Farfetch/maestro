1920df4eebdeb9d4bba425bdaf84009f855bd465
André Santos <andre.anselmosantos@farfetch.com>
2023-09-07 16:10:15 +0100

    lint fix

--- web/frontend/src/components/Run/StatusMonitor/RunStatusMonitor.js
+++ web/frontend/src/components/Run/StatusMonitor/RunStatusMonitor.js
@@ -43,7 +43,7 @@ const RunStatusStepsMonitor = ({ runId, children }) => {
     }, updateRunInterval);
 
     return () => clearInterval(interval);
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [runId, isIntervalLoading]);
 
   return children({ isLoading, run });
