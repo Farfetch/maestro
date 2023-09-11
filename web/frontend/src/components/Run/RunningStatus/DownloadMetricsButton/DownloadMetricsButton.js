0404c89fe9a69d584b5a7d26b3f41ce66fe50f3e
André Santos <andre.anselmosantos@farfetch.com>
2023-09-11 16:29:47 +0100

    fix donwload issue

--- web/frontend/src/components/Run/RunningStatus/DownloadMetricsButton/DownloadMetricsButton.js
+++ web/frontend/src/components/Run/RunningStatus/DownloadMetricsButton/DownloadMetricsButton.js
@@ -3,12 +3,7 @@ import { Button } from "antd";
 import { runMetricsDownloadUrl } from "../../../../lib/routes";
 
 const DownloadMetricsButton = ({ runId }) => (
-  <a
-    href={runMetricsDownloadUrl(runId)}
-    target="_blank"
-    download
-    rel="noreferrer"
-  >
+  <a href={runMetricsDownloadUrl(runId)} target="_blank" rel="noreferrer">
     <Button type="primary">Download metrics</Button>
   </a>
 );
