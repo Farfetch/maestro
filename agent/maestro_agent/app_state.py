b807902280b772884d577facd3e304ad31598f55
crazyplayy <48414755+crazyplayy@users.noreply.github.com>
2023-09-15 11:06:18 +0100

    Feat: Agents variables values are now displayed in log when the Agent is initiated (#822)

--- agent/maestro_agent/app_state.py
+++ agent/maestro_agent/app_state.py
@@ -3,6 +3,18 @@ from maestro_agent.services.maestro_api.agent import AgentApi, AgentStatus
 from maestro_agent.services.host import HostService
 from maestro_agent.services.jmeter.container import JmeterContainerStateManager
 from maestro_agent.logging import Logger
+from maestro_agent.settings import (
+    DEFAULT_LOGGER_NAME,
+    ENABLE_MAESTRO_API_HANDLER,
+    MAESTRO_CSV_WRITER_ENABLED,
+    MAESTRO_METRICS_PROCESSING_BULK_SIZE,
+    MAESTRO_METRICS_PROCESSING_WORKERS,
+    MAESTRO_AGENT_WAITING_SERVERS_TIMEOUT,
+    JMETER_IMAGE_BASE_REPO,
+    JMETER_IMAGE_BASE_VERSION,
+    JMETER_BASE_IMAGE,
+    JMETER_IMAGE_HEAP,
+)
 
 
 class ApplicationState:
@@ -22,7 +34,16 @@ class ApplicationState:
         JmeterContainerStateManager.clean_old_containers()
 
         Logger.info(
-            f"agent_id={agent.id}, ip={agent.ip}, hostname={agent.hostname}",
+            f"agent_id={agent.id} | ip={agent.ip} | hostname={agent.hostname}  | "
+            f"logger_name={DEFAULT_LOGGER_NAME} | "
+            f"api_handler_enabled={ENABLE_MAESTRO_API_HANDLER} | "
+            f"csv_writer_enabled={MAESTRO_CSV_WRITER_ENABLED} | "
+            f"bulk_size={MAESTRO_METRICS_PROCESSING_BULK_SIZE} | "
+            f"workers={MAESTRO_METRICS_PROCESSING_WORKERS} | "
+            f"servers_timeout={MAESTRO_AGENT_WAITING_SERVERS_TIMEOUT} | "
+            f"image_repo={JMETER_IMAGE_BASE_REPO} | "
+            f"image_version={JMETER_IMAGE_BASE_VERSION} | "
+            f"image={JMETER_BASE_IMAGE} | image_heap={JMETER_IMAGE_HEAP}"
         )
 
         ApplicationState.agent = agent
