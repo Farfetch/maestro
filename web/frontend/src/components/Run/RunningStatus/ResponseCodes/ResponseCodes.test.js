ccc76d094b16bd4d8a68ab15b622caa9dbb4a0ad
André Santos <andre.anselmosantos@farfetch.com>
2023-09-26 11:27:39 +0100

    Refactors tests, updates snapshots

--- web/frontend/src/components/Run/RunningStatus/ResponseCodes/ResponseCodes.test.js
+++ web/frontend/src/components/Run/RunningStatus/ResponseCodes/ResponseCodes.test.js
@@ -4,10 +4,13 @@ import React from "react";
 import ResponseCodes from "./ResponseCodes";
 
 describe("ResponseCodes", () => {
-  const mockRunId = "123";
+  const mockErrorCodes = {
+    500: [{ key: "error1", errorsCount: 5, responses: [] }],
+    501: [{ key: "error2", errorsCount: 3, responses: [] }]
+  };
 
   test("should render ResponseCodes component", () => {
-    const rendered = render(<ResponseCodes runId={mockRunId} />);
+    const rendered = render(<ResponseCodes errorCodes={mockErrorCodes} />);
     const component = rendered.container;
     expect(component.outerHTML).toMatchSnapshot();
   });
